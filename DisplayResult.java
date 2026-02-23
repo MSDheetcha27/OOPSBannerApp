0a3415fafa5681d1f53028f5068f5e05b68f7518
MSDheetcha27 <dm3238@srmist.edu.in>
2026-02-23 09:08:14 +0530

    week 1 completed

---
+++ DisplayResult.java
@@ -0,0 +1,22 @@
+class DisplayResult {
+    public static void main(String[] args) {
+     
+   
+        String name = "Sam";
+     
+       
+        int rollNumber = 1;
+     
+        
+        double percentMarks = 99.99;
+     
+        
+        char result = 'P';
+        
+        
+        System.out.println("Displaying Result:\n" +name+ " with Roll Number " +
+rollNumber+ " has Scored " +percentMarks+ 
+"% Marks and Result is " +result);
+    }
+}
+
